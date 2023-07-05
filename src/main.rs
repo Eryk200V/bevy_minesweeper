80ba323c9da27c82c03e3456d237d461c6c51324
MrBomba94 <lrevpu@gmail.com>
2023-07-06 00:04:14 +0200

    m

--- src/main.rs
+++ src/main.rs
@@ -166,7 +166,7 @@ fn main() {
                         Window{
                             title: "Minesweeper".to_string(),
                             resolution: WindowResolution::new(EAZY_BOARD_SIZE.1 as f32 * TILE_SIZE, EAZY_BOARD_SIZE.0 as f32 * TILE_SIZE + TILE_SIZE),
-                            canvas: Some("#bevy".to_string()),
+                            canvas: Some("#bevy".to_owned()),
                             resizable: false,
                             ..default()
                         }),
